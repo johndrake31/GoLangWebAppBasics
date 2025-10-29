711a68202f0d65efb760d9b045f2863a71fcb76c
peterturn <peterturnmay@gmail.com>
2025-10-29 12:57:58 -0500

    add comments for readability

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,6 +1,7 @@
 package render
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -12,9 +13,9 @@ var templateDir = "./templates"
 // RenderTemplate renders a template using the html/template package
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	path := filepath.Join(templateDir, tmpl)
-	
-	t, err := template.ParseFiles(path)
-	
+
+	t, err := template.ParseFiles(path, filepath.Join(templateDir, "base.layout.tmpl"))
+
 	// Handle error if template parsing fails
 	if err != nil {
 		log.Printf("ParseFiles(%s) failed: %v", path, err)
@@ -27,4 +28,43 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 		http.Error(w, "template execute error", http.StatusInternalServerError)
 		return
 	}
+}
+
+var tc = make(map[string]*template.Template)
+
+// RenderTemplateTest renders a template using a template cache
+func RenderTemplateTest(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	_, inMap := tc[t]
+	if !inMap {
+		// need to create the template if not found
+	} else {
+		// we have it in the map
+		log.Println("using cached template")
+	}
+
+	tmpl = tc[t]
+
+	err = tmpl.Execute(w, nil)
+}
+
+// CreateTemplateCache creates a template cache
+func CreateTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("%s/%s", templateDir, t),
+		fmt.Sprintf("%s/%s", templateDir, "base.layout.tmpl"),
+	}
+
+	// parse the template files
+	tmpl, err := template.ParseFiles(templates...)
+	// Handle error if template parsing fails
+	if err != nil {
+		log.Printf("ParseFiles(%s) failed: %v", tmpl, err)
+		http.Error(w, "template not found", http.StatusInternalServerError)
+		return
+	}
+
+	return nil
 }
